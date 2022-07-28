657c808b47427e8245099ce7329b15c26f709373
Talom <franklinfrost14@gmail.com>
2022-07-28 17:22:22 +0100

    feat: add some sample movie data in server/test/data/users.ts for any test

--- server/test/data/users.ts
+++ server/test/data/users.ts
@@ -4,3 +4,55 @@ export default [
     email: "hugo@starter.com",
   },
 ];
+
+// some data to fill in the database for any test
+export const sampleMovieData = [
+  {
+    id: 1,
+    actorName: "Chris Pratt",
+    actorImage: "/83o3koL82jt30EJ0rz4Bnzrt2dd.jpg",
+    movie: "Jurassic World Dominion",
+    movieImage: "/kAVRgw7GgK1CfYEJq8ME6EvRIgU.jpg",
+    hasPlayed: true,
+  },
+  {
+    id: 2,
+    actorName: "Laura Dern",
+    actorImage: "/gB9PnGEvxKg33OSlcqptQwTBwPE.jpg",
+    movie: "Jurassic World Dominion",
+    movieImage: "/kAVRgw7GgK1CfYEJq8ME6EvRIgU.jpg",
+    hasPlayed: true,
+  },
+  {
+    id: 3,
+    actorName: "Sam Neill",
+    actorImage: "/bNZ03phkLewj8eUR6mGbKZ7jtmv.jpg",
+    movie: "Jurassic World Dominion",
+    movieImage: "/kAVRgw7GgK1CfYEJq8ME6EvRIgU.jpg",
+    hasPlayed: true,
+  },
+  {
+    id: 4,
+    actorName: "Sam Neill",
+    actorImage: "/bNZ03phkLewj8eUR6mGbKZ7jtmv.jpg",
+    movie: "Thor: Love and Thunder",
+    movieImage: "/p1F51Lvj3sMopG948F5HsBbl43C.jpg",
+    hasPlayed: false,
+  },
+  {
+    id: 5,
+    actorName: "Mira bell",
+    actorImage: "/w12x0SI8acaWEKFid91UMgPyTZw.jpg",
+    movie: "Thor: Love and Thunder",
+    movieImage: "/pIkRyD18kl4FhoCNQuWxWu5cBLM.jpg",
+    hasPlayed: false,
+  },
+  {
+    id: 6,
+    actorName: "Justice Smith",
+    actorImage: "/3UoUT44XYWoWNsZaqHzN3w48gZL.jpg",
+    movie: "Top Gun: Maverick",
+    movieImage: "/62HCnUTziyWcpDaBO2i1DX17ljH.jpg",
+    hasPlayed: false,
+  },
+];
