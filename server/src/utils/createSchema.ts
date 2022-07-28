b1c8b3b35b5750d85dea4ea51bdfe54de520dc48
Talom <franklinfrost14@gmail.com>
2022-07-28 13:22:03 +0100

    feat: add question resolver in createSchema.ts file

--- server/src/utils/createSchema.ts
+++ server/src/utils/createSchema.ts
@@ -1,9 +1,10 @@
 import { GraphQLSchema } from "graphql";
+import { QuestionResolver } from "../resolvers/question";
 import { buildSchema } from "type-graphql";
 import { userResolver } from "../resolvers/user";
 
 export const createSchema = (): Promise<GraphQLSchema> =>
   buildSchema({
-    resolvers: [userResolver],
+    resolvers: [userResolver, QuestionResolver],
     validate: false,
   });
